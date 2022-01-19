4ccd5900ae8e751b8a83751ae0e90e6a6d5694e8
Davron Abdukhakimov <davron2806@gmail.com>
2022-01-20 01:09:59 +0500

    blackjack

--- blackjack/app.js
+++ blackjack/app.js
@@ -1,19 +1,45 @@
-let btn = document.getElementById("start-btn")
-let firstCard = 10
-let secondCard = 4
-const cards = [firstCard, secondCard];
-let sum = firstCard + secondCard
+let cards = [];
+let sum = 0
 let hasBlackjack = false
 let isAlive = true
 let message = ""
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.querySelector("#cards-el");
-
 let messageEl = document.getElementById("message-el");
+let player = {
+    name: "John",
+    chips: 145
+}
 
-btn.addEventListener("click", function () {
-    sumEl.textContent = "Sum: " + sum
+let playerEl = document.querySelector("#player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomCard = Math.floor(Math.random() * 13) + 1
+    if (randomCard === 1) {
+        return 11;
+    } else if (randomCard >= 11) {
+        return 10
+    }
+    else {
+        return randomCard
+    }
+}
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard
+
+    renderGame()
+}
+
+
+function renderGame() {
     cardsEl.textContent = "Cards: "
+    sumEl.textContent = "Sum: " + sum
     for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
     }
@@ -29,26 +55,13 @@ btn.addEventListener("click", function () {
 
     messageEl.textContent = message;
 }
-)
+
 
 function newCard() {
-    let a = 7;
-    sum += a;
-    cards.push(a)
-    sumEl.textContent = "Sum: " + sum
-    cardsEl.textContent = "Cards: "
-    for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
+    if (isAlive === true && hasBlackjack === false) {
+        let a = getRandomCard();
+        sum += a;
+        cards.push(a)
+        renderGame()
     }
-    if (sum < 21) {
-        message = "Do you want to draw a new game?"
-    } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack!"
-        hasBlackjack = true;
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
-    }
-
-    messageEl.textContent = message;
 }
